d9a266ff88c91398281400a2b1ed1d75b334dd2c
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-02-15 11:22:28 +0100

    Changes in content and javascript

--- Privacy-Video.js
+++ Privacy-Video.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
-  // when we click on the h1 it should alert the user
-  $("#myIdHere").click(function(){
-    var num = "<h1>My header</h1>";
-    var currentHTML = $("#myIdHere").html();
-    $("#myIdHere").html(num + currentHTML);
-  });
 
-  // document.getElementById("myIdHere").innerHTML = num;
+
+var answerOne = "Ja"
+var answerTwo = "Nee"
+
+
+	if (answerOne === "Ja"){
+    	alert = ("Super gevaarlijk!");
+} 
+	else  if {
+    	(answerOne === "Nee"){
+   		alert = ("Goed bezig!");	
+}
+
+
+$("#button").click(function() {
+
+
+$("#button").show("Super Gevaarlijk!");
+$("#buttin").show("Goed bezig!");
+
 });
+  
