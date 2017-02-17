308c68af1b48311ae0a9a78c114a4e965b981206
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-02-17 09:17:55 +0100

    Add an alert on buttons #2

--- Privacy-Video.js
+++ Privacy-Video.js
@@ -1,24 +1,18 @@
 $(document).ready(function () {
 
-
-var answerOne = "Ja"
-var answerTwo = "Nee"
-
-
-	if (answerOne === "Ja"){
-    	alert = ("Super gevaarlijk!");
-} 
-	else  if {
-    	(answerOne === "Nee"){
-   		alert = ("Goed bezig!");	
-}
-
-
-$("#button").click(function() {
-
-
-$("#button").show("Super Gevaarlijk!");
-$("#buttin").show("Goed bezig!");
+$("#answerOne").click(function() {
+	alert("Super gevaarlijk!");
 
 });
-  
+
+
+$("#answerTwo").click(function() {
+	alert("Goed bezig!");
+
+});
+
+});
+
+
+
+
