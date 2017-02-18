0e4fd00071f6679c5cdf5998f00c9e9d85613bda
Marjolein Koudijs <koudijs.marjolein@gmail.com>
2017-02-18 20:00:02 +0100

    Added Donation buttons

--- js/contribute.js
+++ js/contribute.js
@@ -1,1 +1,15 @@
 // Marjolein Koudijs
+$(document).ready(function () {
+	
+	//Alert when donating
+	$("#euro5").click(function() {
+		alert("Bedankt voor jouw 5 euro!");
+	});
+	$("#euro10").click(function() {
+		alert("Bedankt voor jouw 10 euro!");
+	});
+	$("#euro20").click(function() {
+		alert("Bedankt voor jouw 20 euro!");
+	});
+
+});
