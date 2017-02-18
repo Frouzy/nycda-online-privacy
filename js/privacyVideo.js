b86759e42ad82495331a8b62580504cd3122db81
Marjolein Koudijs <koudijs.marjolein@gmail.com>
2017-02-19 00:50:06 +0100

    Complete re-do of privacyVideo-stack

--- js/privacyVideo.js
+++ js/privacyVideo.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
 	$("#answerOne").click(function() {
 		alert("Super gevaarlijk!");
-
 	});
 	$("#answerTwo").click(function() {
 		alert("Goed bezig!");
-
 	});
+
+	$("#dropbtnKind").click(function(){
+        $("#kind").slideToggle("slow");
+    });
+    $("#dropbtnVolw").click(function(){
+        $("#volw").slideToggle("slow");
+    });
+
 });
