0e31e20a4aae24f517191227706f0e7eed05eeee
Marjolein Koudijs <koudijs.marjolein@gmail.com>
2017-02-18 21:44:09 +0100

    Added Nieuwsbrief javascript

--- js/contribute.js
+++ js/contribute.js
@@ -1,7 +1,28 @@
 // Marjolein Koudijs
 $(document).ready(function () {
 	
-	//Alert when donating
+	//Empty input fields Nieuwbrief on click
+	$("#inputName").click(function() {
+		this.value = "";
+	});
+	$("#inputMail").click(function() {
+		this.value = "";
+	});
+
+	// Check for empty fields and warn user
+	$("#verstuur").click(function() {
+		var naam = $("#inputName").val();
+		var mail = $("#inputMail").val();
+		if (naam == "" || naam == "Naam") {
+			alert("Vul je naam in.");
+		} else if (mail == "" || mail == "Emailadres") {
+			alert("Vul je emailadres in.");
+		} else {
+			alert("Bedankt voor je aanmelding, "+ naam);
+		}
+	});
+
+	//Alert when donating on click
 	$("#euro5").click(function() {
 		alert("Bedankt voor jouw 5 euro!");
 	});
